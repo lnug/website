6e07bdc7a8e2936eb96b288aea02ead97390f714
chiragdoctor <chirag.doctor@gmail.com>
2015-08-21 08:18:47 +0100

    initial render of list of events.

--- js/related.js
+++ js/related.js
@@ -1,24 +1,18 @@
 var readMeUrl = "https://api.github.com/repos/lnug/related-meetups/readme";
-
+var url = "http://raw.github.com/lnug/related-meetups/master/README.md";
 function onSucess(data){
-    //var events = data.trim().split('#');
-    //console.log(events);
-    //var text = '';
-    //
-    //for(i =1; i <= events.length-1; i++){
-    //    text += '<div class=\"event\">'+  events[i]  + '</div>';
-    //}
+    console.log(data);
+    var converter = new showdown.Converter(),
+        html      = converter.makeHtml(data);
 
-    $('.events').html(data);
-
-
+    $('.events').html(html);
 }
 
 function makeRequest(url){
     $.ajax({
         url: url,
         type: "GET",
-        beforeSend: function(xhr){xhr.setRequestHeader('Accept', 'application/vnd.github.VERSION.html');},
+        beforeSend: function(xhr){xhr.setRequestHeader('Accept', 'application/vnd.github.VERSION.raw');},
         success: onSucess
     });
 }
