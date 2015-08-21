464263153137c29a83fc7ec69598f07c3fe3505e
chiragdoctor <chirag.doctor@gmail.com>
2015-08-21 08:25:16 +0100

    parsing links properly.

--- js/related.js
+++ js/related.js
@@ -2,8 +2,9 @@ var readMeUrl = "https://api.github.com/repos/lnug/related-meetups/readme";
 var url = "http://raw.github.com/lnug/related-meetups/master/README.md";
 function onSucess(data){
     console.log(data);
-    var converter = new showdown.Converter(),
-        html      = converter.makeHtml(data);
+    var converter = new showdown.Converter();
+    converter.setOption('simplifiedAutoLink', 'true');
+    var    html      = converter.makeHtml(data);
 
     $('.events').html(html);
 }
