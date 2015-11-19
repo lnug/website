a16547bed93005954b7da9a3e9a978e8bdb0411e
Adam Davis <adam@admataz.com>
2015-11-19 01:06:53 +0000

    neatened code of conduct, used existing template, and added to build for monthly data

--- bin/build-homepage.js
+++ bin/build-homepage.js
@@ -15,7 +15,7 @@ var titoLink = require('../lib/tito-link');
 var indexTemplate = fs.readFileSync('./templates/index.html', 'utf8');
 var speakerTemplate = fs.readFileSync('./templates/speaker.html', 'utf8');
 var sponsorsTemplate = fs.readFileSync('./sponsors.html', 'utf8');
-var cocTemplate = fs.readFileSync('./code-of-conduct.html', 'utf8');
+var cocContent = fs.readFileSync('./code-of-conduct-content.html', 'utf8');
 
 /**
  * Turns speaker object into selector object to be used by sizlate.
@@ -57,5 +57,15 @@ var out_sponsors = sizlate.doRender(sponsorsTemplate, {
   }
 });
 
+var out_coc = sizlate.doRender(indexTemplate, {
+  '.lnug-nextmeetup': nextEvent(),
+  '.lnug-content': cocContent,
+  '.talk-label': 'Code of conduct at LNUG events',
+  '#lnug-tkt': {
+      href: titoLink()
+  }
+});
+
 fs.writeFileSync('./index.html', out, 'utf8');
 fs.writeFileSync('./sponsors.html', out_sponsors, 'utf8');
+fs.writeFileSync('./code-of-conduct.html', out_coc, 'utf8');
