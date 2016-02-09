a20ca806342ea633baea733fe06752d633145928
Ricardo Quintas <rqcenter-web@yahoo.com>
2016-02-09 20:21:46 +0100

    adding image-gallery main logic

--- bin/build-markup.js
+++ bin/build-markup.js
@@ -13,7 +13,7 @@ var titoLink = require('../lib/tito-link')
 var venue = require('../data/venues/makers.json')
 
 var eventDate = nextEvent()
-// commenting out becuase this gets overriden below. This needs to get moved into separate files so it can be easily switched out.
+    // commenting out becuase this gets overriden below. This needs to get moved into separate files so it can be easily switched out.
 
 // not in use yet.
 // var sponsors = {
@@ -62,89 +62,101 @@ var eventDate = nextEvent()
 // }
 
 var spec = {
-  '/index.html': {
-    page: 'home',
-    spec: {
-      '.lnug-ticket': {
-        component: 'ticket',
-        data: {
-          '.lnug-nextmeetup': eventDate,
-          '.venue': venue.title,
-          '.detail': venue.detail,
+    '/index.html': {
+        page: 'home',
+        spec: {
+            '.lnug-ticket': {
+                component: 'ticket',
+                data: {
+                    '.lnug-nextmeetup': eventDate,
+                    '.venue': venue.title,
+                    '.detail': venue.detail,
 
-          'address': venue.address.join('<br />'),
-          '.address a': {
-            href: 'https://www.google.co.uk/maps/search/' + venue.address.join(', ')
-          },
-          'a.cta': {
-            'href': titoLink()
-          }
+                    'address': venue.address.join('<br />'),
+                    '.address a': {
+                        href: 'https://www.google.co.uk/maps/search/' + venue.address.join(', ')
+                    },
+                    'a.cta': {
+                        'href': titoLink()
+                    }
+                }
+            },
+            '.lnug-content': {
+                component: 'speaker',
+                data: require('../lib/speaker-selectors')
+            },
+            '.lnug-mailing-list': {
+                component: 'sign-up'
+            }
         }
-      },
-      '.lnug-content': {
-        component: 'speaker',
-        data: require('../lib/speaker-selectors')
-      },
-      '.lnug-mailing-list': {
-        component: 'sign-up'
-      }
+    },
+    '/archive.html': {
+        page: 'archive',
+        spec: {
+            'ul.archive': {
+                component: 'archive',
+                data: require('../lib/archive-selectors')
+            }
+        }
+    },
+    '/code-of-conduct.html': {
+        page: 'code-of-conduct',
+        spec: {
+            'ul.archive': {
+                component: 'archive',
+                data: require('../lib/archive-selectors')
+            }
+        }
+    },
+    '/image-gallery.html': {
+        page: 'image-gallery',
+        spec: {
+            '.image-gallery': {
+                component: 'image-gallery',
+                data: require('../lib/image-gallery')
+            },
+        }
+    },
+    '/speak.html': {
+        page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/speakers/master/README.md')
+    },
+    '/sponsor.html': {
+        page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/resources/master/sponsors.md')
+            // there is a bug in speclate which needs fixing to get this working, for the time being sponsors are hard coded in the markdown file.
+            //   spec: {
+            //     '.gold-sponsor': {
+            //         component: 'sponsor',
+            //         data: sponsorSelectors(sponsors.gold)
+            //     },
+            //     '.silver-sponsor': {
+            //         component: 'sponsor',
+            //         data: sponsorSelectors(sponsors.silver)
+            //     },
+            //     '.bronze-sponsor': {
+            //         component: 'sponsor',
+            //         data: sponsorSelectors(sponsors.bronze)
+            //     },
+            //     // '.community-sponsor': {
+            //     //     component: 'sponsor',
+            //     //     data: sponsorSelectors(sponsors.community)
+            //     // }
+            //   }
+    },
+    '/contact.html': {
+        page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/feedback/master/ORGANISERS.md')
+    },
+    '/related-meetups.html': {
+        page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/related-meetups/master/README.md')
     }
-  },
-  '/archive.html': {
-    page: 'archive',
-    spec: {
-      'ul.archive': {
-        component: 'archive',
-        data: require('../lib/archive-selectors')
-      }
-    }
-  },
-  '/code-of-conduct.html': {
-    page: 'code-of-conduct'
-  },
-  '/image-gallery.html': {
-    page: 'image-gallery'
-  },
-  '/speak.html': {
-    page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/speakers/master/README.md')
-  },
-  '/sponsor.html': {
-    page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/resources/master/sponsors.md')
-    // there is a bug in speclate which needs fixing to get this working, for the time being sponsors are hard coded in the markdown file.
-    //   spec: {
-    //     '.gold-sponsor': {
-    //         component: 'sponsor',
-    //         data: sponsorSelectors(sponsors.gold)
-    //     },
-    //     '.silver-sponsor': {
-    //         component: 'sponsor',
-    //         data: sponsorSelectors(sponsors.silver)
-    //     },
-    //     '.bronze-sponsor': {
-    //         component: 'sponsor',
-    //         data: sponsorSelectors(sponsors.bronze)
-    //     },
-    //     // '.community-sponsor': {
-    //     //     component: 'sponsor',
-    //     //     data: sponsorSelectors(sponsors.community)
-    //     // }
-    //   }
-  },
-  '/contact.html': {
-    page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/feedback/master/ORGANISERS.md')
-  },
-  '/related-meetups.html': {
-    page: require('../lib/markdown')('https://raw.githubusercontent.com/lnug/related-meetups/master/README.md')
-  }
 }
 
 generateMaps(venue.location)
 speclate.generate(spec)
 speclate.appCache(spec, [
-  '/css.css',
-  '/app-cache-nanny.js',
-  '/',
-  '/images/lnug-logo.svg',
-  '/images/maps/thin.png',
-  '/images/maps/wide.png'
+    '/css.css',
+    '/app-cache-nanny.js',
+    '/',
+    '/images/lnug-logo.svg',
+    '/images/maps/thin.png',
+    '/images/maps/wide.png'
 ])
