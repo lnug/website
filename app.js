1a97062f427baa08ef59bdf07ed5594db459bb30
Ricardo Quintas <rqcenter-web@yahoo.com>
2016-02-10 19:39:25 +0100

    added Magnific Popup js

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ $(function() {
     $("img.lazy").lazyload({
         effect: "fadeIn"
     });
+
+    $('#gallery').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+    });
+
 });
