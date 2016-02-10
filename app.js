ed98db547461fb69775367bd90d582adc7ce5261
Ricardo Quintas <rqcenter-web@yahoo.com>
2016-02-10 22:03:21 +0100

    adding images and improve layout with Masonry

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ $(function() {
         },
     });
 
+
+    $('#gallery').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+    });
+
+
+    $('#gallery').masonry({
+        columnWidth: 80,
+        itemSelector: '.single-image'
+    });
+
 });
