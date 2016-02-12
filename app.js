9da45a5f0693f8805111b88bd98307edf0d2ec34
Ricardo Quintas <rqcenter-web@yahoo.com>
2016-02-12 02:37:21 +0100

    removing masonry and adding home page html

--- app.js
+++ app.js
@@ -3,27 +3,47 @@ $(function() {
         effect: "fadeIn"
     });
 
-    $('#gallery').magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
-        type: 'image',
-        gallery: {
-            enabled: true
-        },
-    });
+    // we shouldn't apply the magnificPopup to smaller devices 
+    if ($(window).width() > 950) {
+        $('#gallery').magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+        });
+    }
+
+    if ($("#home-gallery").length > 0) {
+        $.getJSON("image-gallery.json", function(data) {
+            if (data.spec["section#gallery"].data) {
+                var images = data.spec["section#gallery"].data;
+                var img1, img2, img3;
+                while (true) {
+                    img1 = Math.floor((Math.random() * images.length));
+                    img2 = Math.floor((Math.random() * images.length));
+                    img3 = Math.floor((Math.random() * images.length));
+                    if (img1 !== img2 && img1 !== img3 && img2 !== img3) {
+                        break;
+                    }
+                }
 
 
-    $('#gallery').magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
-        type: 'image',
-        gallery: {
-            enabled: true
-        },
-    });
+                // var items = [];
+                // $.each(data, function(key, val) {
+                //     items.push("<li id='" + key + "'>" + val + "</li>");
+                // });
 
 
-    $('#gallery').masonry({
-        columnWidth: 80,
-        itemSelector: '.single-image'
-    });
+                $("#home-gallery").append("<img src='" + images[img1].a.href + "'>");
+                $("#home-gallery").append("<img src='" + images[img2].a.href + "'>");
+                $("#home-gallery").append("<img src='" + images[img3].a.href + "'>");
+                // $("<ul/>", {
+                //     "class": "my-new-list",
+                //     html: items.join("")
+                // }).appendTo("body");
+            }
+        });
+    }
 
 });
