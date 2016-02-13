a2a2eba88106d66b6183c3e6fb66f453bfaa4650
Ricardo Quintas <rqcenter-web@yahoo.com>
2016-02-13 21:01:18 +0100

    adding node-easyimage to process thumbs

---
+++ bin/make-thumbs.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+'use strict'
+
+var easyimage = require('easyimage')
+var path = require('path')
+var fs = require('fs')
+
+fs.readdir('./images/gallery', function (err, items) {
+  if (err) throw err
+  for (var i = 0; i < items.length; i++) {
+    // console.log(path.dirname())
+    easyimage.rescrop({
+      src: './images/gallery/' + items[i],
+      dst: './images/gallery/thumbs/' + items[i],
+      width: 500,
+      height: 500,
+      cropwidth: 350,
+      cropheight: 350,
+      x: 0,
+      y: 0
+    }).then(
+      function (image) {
+        console.log('Resized and cropped: ' + image.width + ' x ' + image.height)
+      },
+      function (err) {
+        console.log(err)
+      }
+    )
+  }
+})
