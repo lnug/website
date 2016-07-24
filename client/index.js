22a120a4f5d203c8424834c45551a90e0a8fcaca
Simon McManus <mcmanus.simon@gmail.com>
2016-07-24 08:24:30 +0100

    ADDING re render

--- client/index.js
+++ client/index.js
@@ -1,15 +1,26 @@
-var spec = require('../spec')
+
+window.spec = require('../spec')
 var router = require('speclate-router')
+
+
+    window.pageRender = require('speclate-router/page-render')
 var appCacheNanny = require('appcache-nanny')
 var analytics = require('ga-browser')(window)
 
 window.$ = require('jquery')
 router(spec, {
   before: function () {
-    $('nav a.active').removeClass('active')
+    $('nav a.active, footer a.active').removeClass('active')
+    $('#container').addClass('hide');
+         $('html,body').animate({ scrollTop: 0 }, 400);
+
   },
   after: function () {
-    $('html,body').scrollTop($('#container'))
+    //$('html,body').scrollTop($('#container'))
+
+     setTimeout(function() {
+        $('#container').removeClass('hide');
+    }, 100);
     analytics('send', 'pageview', {
       page: window.location.pathname,
       title: document.title
@@ -27,27 +38,32 @@ analytics('send', 'pageview')
 
 appCacheNanny.start()
 appCacheNanny.on('updateready', function (a, b, c) {
-
-    $('#notice').addClass('on');
-    var duration = 30000;
-    var refreshTime = Date.now() + duration
-    var refreshIntervalId = setInterval(function() {
-        var remaining = refreshTime  -  Date.now() ;
-        $('#notice .countdown').html( Math.ceil(remaining / 1000))
-    }, 1000);
+    // get latest spec
 
 
-    var hideDuration = 1000;
-    setTimeout(function() {
-        clearInterval(refreshIntervalId);
+//    var page = (window.location.pathname === '/') ? '/index.html' : window.location.pathname
 
-        $('#notice').addClass('hide');
+    pageRender(spec[window.location.pathname], {});
+    // $('#notice').addClass('on');
+    // var duration = 30000;
+    // var refreshTime = Date.now() + duration
+    // var refreshIntervalId = setInterval(function() {
+    //     var remaining = refreshTime  -  Date.now() ;
+    //     $('#notice .countdown').html( Math.ceil(remaining / 1000))
+    // }, 1000);
 
-        setTimeout(function() {
-            location.reload();
-        }, hideDuration)
 
-    }, duration - hideDuration);
+    // var hideDuration = 1000;
+    // setTimeout(function() {
+    //     clearInterval(refreshIntervalId);
+
+    //     $('#notice').addClass('hide');
+
+    //     setTimeout(function() {
+    //         location.reload();
+    //     }, hideDuration)
+
+    // }, duration - hideDuration);
 
 
     // var refreshTime = Date.now() + 30000
