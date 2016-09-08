85541d2bde7c0b374707f4b693ac318c4b5db373
Simon McManus <mcmanus.simon@gmail.com>
2016-09-08 17:50:01 +0100

    tweaks to client

--- client/index.js
+++ client/index.js
@@ -21,39 +21,29 @@ speclateRouter(spec, {
 
 
 
-
 window.$ = require('jquery')
 
-// var request = new Request('./client/spec.json', {
-//   headers: new Headers({
-//     'Content-Type': 'application/json'
-//   })
-// })
 
-
-    router({
-      before: function () {
-        $('nav a.active').removeClass('active')
-      },
-      after: function () {
-        $('html,body').scrollTop($('#container'))
-        analytics('send', 'pageview', {
-          page: window.location.pathname,
-          title: document.title
-        })
-      },
-      error: function (err) {
-        if (err) {
-          location.reload()
-        }
-      }
+router({
+  before: function () {
+    $('nav a.active').removeClass('active')
+  },
+  after: function () {
+    $('html,body').scrollTop($('#container'))
+    analytics('send', 'pageview', {
+      page: window.location.pathname,
+      title: document.title
     })
+  },
+  error: function (err) {
+    if (err) {
+      location.reload()
+    }
+  }
+})
 
-
-
-// analytics('create', 'UA-2845245-14', 'auto')
-// analytics('send', 'pageview')
-
+analytics('create', 'UA-2845245-14', 'auto')
+analytics('send', 'pageview')
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
@@ -64,3 +54,4 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err)
   })
 }
+
