45ea6057099dd814fc484b720e01d3e2cc0df056
Simon McManus <mcmanus.simon@gmail.com>
2016-09-08 17:53:40 +0100

    removed files from service worker as they are now in the spec

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,21 +1,5 @@
 'use strict'
-
+// version 1.4
 var serviceWorker = require('speclate-service-worker')
 var spec = require('../spec')
-var version = '1.3'
-console.log('version is', version);
-var extraFiles = [
-  '/',
-  'css.css',
-  'client/index-compiled.js',
-  'client/spec.json',
-  'images/lnug-logo-monochrome.svg',
-  'images/lnug-logo.svg',
-  'images/maps/thin.png',
-  'images/maps/wide.png',
-  'images/favicon/favicon-16x16.png',
-  'images/favicon/favicon-128.png'
-]
-
-
-serviceWorker(spec, extraFiles)
+serviceWorker(spec)
