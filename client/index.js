a9c86810cadc2b9d12b155777c717109795f9fdb
Simon McManus <mcmanus.simon@gmail.com>
2016-09-20 09:38:29 +0100

    adding back log on register

--- client/index.js
+++ client/index.js
@@ -29,7 +29,7 @@ analytics('send', 'pageview')
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
     // Registration was successful
-//    console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    console.log('ServiceWorker registration successful with scope: ', registration.scope)
   }).catch(function (err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err)
