3e75516626c036980a82dfd759629d13d12e29bc
Simon McManus <mcmanus.simon@gmail.com>
2018-06-19 11:20:49 +0100

    remove new version available prompt

--- client/index.js
+++ client/index.js
@@ -28,38 +28,10 @@ router({
 ga('create', 'UA-2845245-14', 'auto')
 ga('send', 'pageview')
 
-var newVersionAvailable = function (newWorker) {
-  $('header').append('<div class="new-version">There is a new version of the LNUG site avaialble.<a href="/">upgrade now</a></div>')
-  $('header div.new-version a').on('click', { newWorker: newWorker }, (event) => {
-    event.data.newWorker.postMessage({ action: 'skipWaiting' })
-    $('header div.new-version').remove()
-  })
-}
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope)
-
-    var newWorker
-    // now we need to detect updates.
-    registration.addEventListener('updatefound', function () {
-      // An updated service worker has appeared in reg.installing!
-      newWorker = registration.installing
-      newWorker.addEventListener('statechange', function () {
-        console.log('nws', newWorker.state)
-        // Has service worker state changed?
-        switch (newWorker.state) {
-          case 'installed':
-
-            // There is a new service worker available, show the notification
-            if (navigator.serviceWorker.controller) {
-              newVersionAvailable(newWorker);
-            }
-            break
-        }
-      })
-    })
   }).catch(function (err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err)
