5af4ceb0b631006b50cbd2ec86703c70d0a8dbd5
Simon McManus <mcmanus.simon@gmail.com>
2019-07-03 15:31:03 +0100

    using date for service worker version becuase the string was getting silly

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,6 +1,6 @@
 'use strict'
 var serviceWorker = require('speclate-service-worker')
 var spec = require('../spec')
-var version = '3.2322s2dd2f'
+var version = '03.07.2019'
 
 serviceWorker(spec, version)
