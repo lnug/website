dc914cd3f2f2726c5de84e7ea867ca9405062bb0
Simon McManus <mcmanus.simon@gmail.com>
2019-07-05 09:35:40 +0100

    tweaking offline

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,6 +1,6 @@
 'use strict'
 var serviceWorker = require('speclate-service-worker')
-var spec = require('../spec')
+var offlineFiles = require('../offline-spec')
 var version = '03.07.2019'
 
-serviceWorker(spec, version)
+serviceWorker(offlineFiles, version)
