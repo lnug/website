732c7a38dc8f0828cecf740378e1125be76be051
Simon McManus <mcmanus.simon@gmail.com>
2019-07-08 14:05:45 +0100

    update sw version

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,6 +1,6 @@
 'use strict'
 var serviceWorker = require('speclate-service-worker')
-var offlineFiles = require('../offline-spec')
-var version = '03.07.2019'
+var offlineFiles = require('./offline-spec')
+var version = '08-01.07.2019'
 
 serviceWorker(offlineFiles, version)
