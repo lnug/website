299d5c923ce32ed2e8ac225605d553e5350aef8c
Adam Davis <adam@admataz.com>
2024-06-15 11:43:56 +0100

    chore: updates for June 2024

---
+++ scripts/current-talks.js
@@ -0,0 +1,35 @@
+const yaml = require('js-yaml');
+
+// Make the GET request to the GitHub API
+fetch('https://api.github.com/repos/lnug/speakers/issues?labels=Accepted%20%26%20Scheduled')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the issues data
+    
+    const talks = data.map(issue => {
+        return {
+          title: issue.title,
+          description: issue.body.split("Speaker:")[0].trim(),
+          speaker: [{
+            name: '',
+            twitter: '', // Remove @ from Twitter handle
+            github: issue.user.login,
+            avatar: issue.user.avatar_url
+          }]
+        };
+      });
+
+      console.log(yaml.dump(talks));
+  })
+  .catch(error => {
+    console.error('Fetching error:', error);
+  });
+
+
+
+  
