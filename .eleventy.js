f0884f7bffa2b1a3a49d6ce7f9678973c6148459
Adam Davis <adam@admataz.com>
2025-08-11 18:08:09 +0100

    udpate eleventy to v3

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,44 @@
-// const sass = require("sass");
 const eleventySass = require("eleventy-sass");
-
-// Add above your Eleventy config
+const pluginHandlebars = require("@11ty/eleventy-plugin-handlebars");
 const markdownIt = require("markdown-it");
 
-// Add within your config module
 const md = new markdownIt({
     html: true,
 });
 
 module.exports = function(eleventyConfig) {
+    // Ensure passthrough copy paths are relative to the input directory
+    eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/manifest.json");
 
-    // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy({"src/img": "img"});
-
-    eleventyConfig.addPassthroughCopy({"src/manifest.json": "manifest.json"});
+    // Add plugins
     eleventyConfig.addPlugin(eleventySass);
+    eleventyConfig.addPlugin(pluginHandlebars);
 
-
+    // Add filters
     eleventyConfig.addFilter("markdown", (content) => {
         return md.render(content);
     });
 
-    eleventyConfig.addFilter("talkdateformat", function(date){
+    eleventyConfig.addFilter("talkdateformat", function(date) {
         return new Date(date).toLocaleString('en-GB', {
             weekday: 'long',
             year: 'numeric',
             month: 'long',
             day: 'numeric'
         });
-    })
+    });
+
+    // Return configuration object
     return {
         dir: {
             input: "src",
             output: "docs"
-        }
-    }
+        },
+    templateFormats: ["html", "hbs", "liquid", "md"], // Add Handlebars and Liquid
+    markdownTemplateEngine: "liquid", // Use Liquid for Markdown files
+    htmlTemplateEngine: "liquid", // Process .html with Liquid (hbs handled by .hbs files)
+        dataTemplateEngine: "liquid" // Use Liquid for data files
+    };
 };
+
